f3fd4484c5239c0e0d7e54a50c1c67eb44ab4713
Dani <git@danny.works>
2023-05-19 20:55:12 +0000

    Add port

--- ghost-5/entrypoint.sh
+++ ghost-5/entrypoint.sh
@@ -32,7 +32,7 @@ function run() {
     ghost config --port $SERVER_PORT
     ghost config --url $EGGOPT_URL
     ghost config --admin-url $EGGOPT_ADMINURL
-    ghost config --db mysql --dbhost $EGGOPT_DBHOST --dbname $EGGOPT_DBNAME --dbuser $EGGOPT_DBUSER --dbpass $EGGOPT_DBPASS
+    ghost config --db mysql --dbhost $EGGOPT_DBHOST --dbname $EGGOPT_DBNAME --dbuser $EGGOPT_DBUSER --dbpass $EGGOPT_DBPASS --dbport $EGGOPT_DBPORT
     ghost start --dir "$GHOST_INSTALLED" --color true --process local
 }
 
