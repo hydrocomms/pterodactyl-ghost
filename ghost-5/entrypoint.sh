3adb95fd758f96d08753b0c75a08a0fe68766d7d
Dani <git@danny.works>
2023-05-19 21:25:03 +0000

    Consolidate config + don't use ghost start

--- ghost-5/entrypoint.sh
+++ ghost-5/entrypoint.sh
@@ -28,13 +28,8 @@ function run() {
     echo "Starting Ghost.. ($IMAGE_VERSION with CLI $BOLD$(ghost version | tail -1 | cut -d' ' -f3)$NORMAL)"
     echo "To change the public-facing URL or admin URL, please edit the variables in Pterodactyl."
     echo "The port is set to the primary port allocation. To edit this, you will need to modify the primary allocation in Pterodactyl."
-    ghost config --ip 0.0.0.0
-    ghost config --port $SERVER_PORT
-    ghost config --url $EGGOPT_URL
-    ghost config --admin-url $EGGOPT_ADMINURL
-    ghost config --db mysql --dbhost $EGGOPT_DBHOST --dbname $EGGOPT_DBNAME --dbuser $EGGOPT_DBUSER --dbpass $EGGOPT_DBPASS --dbport $EGGOPT_DBPORT
-    ghost config --process local
-    ghost start --dir "$GHOST_INSTALLED" --color true --process local
+    ghost config --process local --port $SERVER_PORT --url $EGGOPT_URL --ip 0.0.0.0 --admin-url $EGGOPT_ADMINURL --db mysql --dbhost $EGGOPT_DBHOST --dbname $EGGOPT_DBNAME --dbuser $EGGOPT_DBUSER --dbpass $EGGOPT_DBPASS --dbport $EGGOPT_DBPORT
+    node current/index.js
 }
 
 if [[ ! "${STARTUP}" == "/usr/bin/false" ]]; then
